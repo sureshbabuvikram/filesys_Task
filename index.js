885fdbf96517222a9b2e41b8fa75724d5a7445ef
sureshbabuvikram <sureshbabuvikram@gmail.com>
2024-06-04 13:05:49 +0530

    tmch

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ const { format } = require('date-fns');
 
 const app = express();
 
+// Set the time zone environment variable
+process.env.TZ = 'UTC';
+
 app.get('/', (req, res) => {
     const currentDate = new Date();
     const today = format(currentDate, "dd-MM-yyyy-HH-mm-ss", { timeZone: "UTC" });
 
     const filePath = `TimeStamp/${today}.txt`;
-    fs.writeFileSync(filePath, `${today}`, 'utf8');
-    let data = fs.readFileSync(filePath, 'utf8');
 
-    try {
-        res.status(200).send(data);
-    } catch (error) {
-        res.status(500).send('Internal server error');
-    }
+    // Write the timestamp to the file asynchronously
+    fs.writeFile(filePath, `${today}`, 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal server error');
+            return;
+        }
+
+        // Read the file asynchronously after it has been written
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Internal server error');
+                return;
+            }
+
+            try {
+                res.status(200).send(data);
+            } catch (error) {
+                res.status(500).send('Internal server error');
+            }
+        });
+    });
 });
 
 // Start the server
